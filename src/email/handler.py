187f95efbe58fb1b4d65e54934760846c3b946f5
Md Najim Ahmed <mnajim.ahmed@petronas.com>
2021-03-05 16:45:19 +0800

    refactoring

--- src/email/handler.py
+++ src/email/handler.py
@@ -3,18 +3,25 @@ import json
 from src.email.sesSender import send_email_with_ses
 
 
+def extract_email_context(event):
+    print("event", event)
+    records = event["Records"]
+    if records is None or len(records) != 1:
+        raise Exception(f"Expected exactly one record,found {len(records)}")
+    email_context = json.loads(records[0]['body'])
+    print("email_context === ", email_context)
+    return email_context
 
 
 def email_handler(event, context):
-    print("event",event)
-    records = event["Records"]
-
-    if records is None or len(records) != 1:
-        raise Exception(f"Expected exactly one record,found {len(records)}")
-    body = json.loads(records[0]['body'])
-    print("body === ", body)
-    send_email_with_ses(body)
+    send_email_with_ses(extract_email_context(event))
     return
+
+
+def email_handler_fallback(event, context):
+    send_email_with_ses(extract_email_context(event))
+    return
+
 
 if __name__ == '__main__':
     email_handler(None)
--- src/email/sesSender.py
+++ src/email/sesSender.py
@@ -7,43 +7,41 @@ from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
 
 
-def downloadFromS3(objectKey):
-    # s3 = boto3.resource('s3')
-    # obj = s3.Object('email-attachment-mediator', objectKey)
-    # return obj.get()['Body'].read()
-
+def download_from_s3(object_key):
+    print(f"downloading {object_key} from S3")
     client = boto3.client('s3')
     bytes_buffer = io.BytesIO()
-    client.download_fileobj(Bucket='email-attachment-mediator', Key=objectKey, Fileobj=bytes_buffer)
+    client.download_fileobj(Bucket='email-attachment-mediator', Key=object_key, Fileobj=bytes_buffer)
     byte_value = bytes_buffer.getvalue()
+    print(f"finished downloading {object_key} from S3")
     return byte_value
 
 
-def send_email_with_ses(data):
-    print("sending email using ses with data = ", data)
+def send_email_with_ses(email_context):
+    print("sending email using ses with data = ", email_context)
     sender = "Timelyship Team <no-reply@timelyship.com>"
-    print("bodyText",data["bodyText"])
-    recipients = data['recipients']
-    print("recipients",recipients)
+    print("bodyText", email_context["bodyText"])
+    recipients = email_context['recipients']
+    print("recipients", recipients)
     aws_region = "ap-southeast-1"
 
     client = boto3.client('ses', region_name=aws_region)
 
     msg = MIMEMultipart('mixed')
-    msg['Subject'] = data["subject"]
+    msg['Subject'] = email_context["subject"]
     msg['From'] = sender
     msg['To'] = ', '.join(recipients)
 
     msg_body = MIMEMultipart('alternative')
 
-    text_part = MIMEText(data['bodyText'].encode("utf-8"), 'plain', "utf-8")
-    html_part = MIMEText(downloadFromS3(data['bodyHtml']).decode(), 'html', "utf-8")
+    text_part = MIMEText(email_context['bodyText'].encode("utf-8"), 'plain', "utf-8")
+    html_part = MIMEText(download_from_s3(email_context['bodyHtml']).decode(), 'html', "utf-8")
 
     msg_body.attach(text_part)
     msg_body.attach(html_part)
 
-    object_key = data['attachment']
-    att = MIMEApplication(downloadFromS3(object_key))
+    object_key = email_context['attachment']
+    att = MIMEApplication(download_from_s3(object_key))
 
     att.add_header('Content-Disposition', 'attachment', filename=object_key)
     msg.attach(msg_body)
